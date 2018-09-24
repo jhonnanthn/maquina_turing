66e2d697137652f549ed588ca0d2612fd33eda93
skyllaion <milena.carolinelt@hotmail.com>
2018-09-24 18:34:28 -0300

    Ajuste requisição

--- service/src/main/java/br/com/usjt/turing/controller/BinaryController.java
+++ service/src/main/java/br/com/usjt/turing/controller/BinaryController.java
@@ -1,5 +1,6 @@
 package br.com.usjt.turing.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,8 @@ import br.com.usjt.turing.model.Turing;
 public class BinaryController {
 
 	private TuringMachine tM1;
+	
+    @CrossOrigin(origins = "*")
 	@RequestMapping(value = "/call", method = RequestMethod.GET, produces = "application/json")
 	public Turing getCall(String numbers ) {
 		tM1 = MachinesLibrary.EqualBinaryWords();

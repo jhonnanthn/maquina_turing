f5913ff6a8ba5cc35b383c9971df23e99e07db57
skyllaion <milena.carolinelt@hotmail.com>
2018-09-24 18:29:09 -0300

    Ajuste requisição

--- service/src/main/java/br/com/usjt/turing/controller/BinaryController.java
+++ service/src/main/java/br/com/usjt/turing/controller/BinaryController.java
@@ -1,6 +1,7 @@
 package br.com.usjt.turing.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.usjt.turing.TuringMachine;
@@ -11,13 +12,9 @@ import br.com.usjt.turing.model.Turing;
 public class BinaryController {
 
 	private TuringMachine tM1;
-
-	public BinaryController() {	
-		tM1 = MachinesLibrary.EqualBinaryWords();
-	}
-	
-	@GetMapping(value="/call")
+	@RequestMapping(value = "/call", method = RequestMethod.GET, produces = "application/json")
 	public Turing getCall(String numbers ) {
+		tM1 = MachinesLibrary.EqualBinaryWords();
 		Turing turing = tM1.Run(numbers, true);
 		return turing;
 	}

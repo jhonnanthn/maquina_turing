a4bb870bb34b57be326681dfe1be4dfccee99d64
skyllaion <milena.carolinelt@hotmail.com>
2018-09-24 08:29:46 -0300

    Ajuste da requisição no service e ajustes de layout no front

--- front/src/assets/js/turing.js
+++ front/src/assets/js/turing.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () =>{
     clickNumber();
+    clearNumber();
     sendNumber();
 });
 
 const clickNumber = () => {
-    
     var el = document.querySelector('.turing__calling-number');
     document.querySelectorAll('.turing__phone-num').forEach((e) => {
         e.addEventListener('click', (item) => {
@@ -14,37 +14,48 @@ const clickNumber = () => {
     });
 }
 
+const clearNumber = () => {
+    document.querySelector('.turing__button-clear').addEventListener('click', (item) => {
+        var el = document.querySelector('.turing__calling-number');
+        el.innerHTML = el.innerText.substring(el.innerTextlength, el.innerText.length - 1);
+    }, false);
+}
+
+const toReadyStateDescription = (state) => {
+    switch (state) {
+        case 0:
+            return 'UNSENT';
+        case 1:
+            return 'OPENED';
+        case 2:
+            return 'HEADERS_RECEIVED';
+        case 3:
+            return 'LOADING';
+        case 4:
+            return 'DONE';
+        default:
+            return '';
+    }
+}
 
 const sendNumber = () => {
-    var retrieve = document.querySelector('.turing__button-call'),
-    toReadyStateDescription = function (state) {
-        switch (state) {
-            case 0:
-                return 'UNSENT';
-            case 1:
-                return 'OPENED';
-            case 2:
-                return 'HEADERS_RECEIVED';
-            case 3:
-                return 'LOADING';
-            case 4:
-                return 'DONE';
-            default:
-                return '';
-        }
-    };
-    retrieve.addEventListener('click', function (e) {
+    var retrieve = document.querySelector('.turing__button-call');
+    retrieve.addEventListener('click', (e) => {
         var el = document.querySelector('.turing__calling-number').innerText,
         url = `http://localhost:8080/maquina_turing/call?numbers=${el}`;
-        var oReq = new XMLHttpRequest();
-        oReq.onload = function () {
-            console.log('Inside the onload event');
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = () => {
+            if(toReadyStateDescription(xhr.readyState) == 'DONE'){
+                if (xhr.status == 200){
+                    const json = JSON.parse(xhr.responseText)
+                    const calling =  document.querySelector('.turing__calling-status');
+                    calling.innerText = json.status;
+                    if(json.status == "Ligação válida")
+                        calling.innerText = `${ calling.innerText} - ${json.pais} para ${json.regiao}.`;
+                }
+            }
         };
-        oReq.onreadystatechange = function () {
-            console.log('Inside the onreadystatechange event with readyState: ' +
-                toReadyStateDescription(oReq.readyState));
-        };
-        oReq.open('GET', url, true);
-        oReq.send();
+        xhr.open('GET', url, true);
+        xhr.send();
     });
 }
